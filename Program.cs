a48b918bf07ee9166380848d83953b4b8310c4f0
altg77 <tiago.santos2712@gmail.com>
2025-10-07 22:54:44 -0300

    newbuilders

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using VigiLant.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddControllers();
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultDatabase");
+builder.Services.AddDbContext<BancoCtx>(opt =>
+{
+    opt.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection));
+});
 
 var app = builder.Build();
 
@@ -9,7 +19,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
