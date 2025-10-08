4180fbd7828baca6b0b2d8a83397a76b219ebf2a
altg77 <tiago.santos2712@gmail.com>
2025-10-07 22:45:35 -0300

    setsBan

--- Data/BancoCtx.cs
+++ Data/BancoCtx.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using VigiLant.Models;
+
 namespace VigiLant.Data
 {
-    public class BancoCtx
+    public class BancoCtx : DbContext
     {
+        public BancoCtx(DbContextOptions<BancoCtx> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Colaborador> Colaboradores { get; set; }
+        public DbSet<Equipamento> Equipamentos { get; set; }
+        public DbSet<Risco> Riscos { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
         
     }
 }
--- obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
+++ obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+385f7409a67a0cad351230f0dc98b94ebaead181")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c7af508947e93b4440f1915fc654c297f25de8")]
 [assembly: System.Reflection.AssemblyProductAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
