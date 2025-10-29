32e796d670bb38e9d7f0e00d621c6e4b3b3fac5b
altg77 <tiago.santos2712@gmail.com>
2025-10-28 23:21:01 -0300

    new

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,8 +17,4 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 }
--- obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
+++ obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1970fa13672954e0806a9525dc88251c1fe94809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c17553b5868a0010e98ac6aec8b1006b8a7379f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
