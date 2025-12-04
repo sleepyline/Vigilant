d1ff4364268eeb5f4111ecd8168751a88f52c8df
altg77 <tiago.santos2712@gmail.com>
2025-12-03 22:43:31 -0300

    BotoesAtualizados

--- obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
+++ obj/Debug/net8.0/VigiLant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fe9fca4bab629e7d5972a6df3589a67a40696b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f84b946a2c5cd78bd09aa30cf8d383df04c4401")]
 [assembly: System.Reflection.AssemblyProductAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VigiLant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
